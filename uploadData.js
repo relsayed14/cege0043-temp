da8800af870c47ca6dd1bf1e74c5c6aa106e6828
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-02-28 19:27:41 -0500

    added comments for clarity and fixed uplaoddata issues

--- uploadData.js
+++ uploadData.js
@@ -1,10 +1,12 @@
 // uploading the answers
 function startDataUpload () {
+	//extract form values (response values)
 	var name = document.getElementById("name").value;
 	var surname = document.getElementById("surname").value;
 	var moduleTitle = document.getElementById("moduleTitle").value;
 	var postString = "name="+name +"&surname="+surname+"&moduleTitle="+moduleTitle;
 	alert (postString);
+	processData(postString);
 }
 
 // global variable to hold the request
@@ -13,9 +15,10 @@ var client;
 function processData(postString) {
 	client = new XMLHttpRequest();
 	client.open('POST','http://developer.cege.ucl.ac.uk:30272/reflectData',true);
+	//notify the server of the data type
 	client.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	client.onreadystatechange = dataUploaded;
-	client.send(postString);
+	client.send(postString)
 }
 
 //create the code to wait for the response from the data server
