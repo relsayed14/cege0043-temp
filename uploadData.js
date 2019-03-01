4694f3cb3b00bea4133b858d14dea7f713490981
Reem Elsayed <reem.elsayed.14@ucl.ac.uk>
2019-02-28 19:15:01 -0500

    AJAX request to make request to send data to server, then to retrieve and process the data once received

--- uploadData.js
+++ uploadData.js
@@ -6,3 +6,24 @@ function startDataUpload () {
 	var postString = "name="+name +"&surname="+surname+"&moduleTitle="+moduleTitle;
 	alert (postString);
 }
+
+// global variable to hold the request
+var client;
+//function to make a request
+function processData(postString) {
+	client = new XMLHttpRequest();
+	client.open('POST','http://developer.cege.ucl.ac.uk:30272/reflectData',true);
+	client.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	client.onreadystatechange = dataUploaded;
+	client.send(postString);
+}
+
+//create the code to wait for the response from the data server
+//and process the response once received
+function dataUploaded() {
+	//this function listens out for the server to say that the data is ready - i.e. has state 4
+	if (client.readyState == 4) {
+		// change the DIV to show the response
+		document.getElementById("dataUploadResult").innerHTML = client.responseText;
+	}
+}
